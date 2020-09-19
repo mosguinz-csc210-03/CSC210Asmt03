4248020972ce7e24521df13eb41a23b3c06928f2
mosguinz <mos.guinz@gmail.com>
2020-09-18 20:41:35 -0700

    Add BMI Table Pro header

--- src/TableBmiPro.java
+++ src/TableBmiPro.java
@@ -4,6 +4,26 @@ public class TableBmiPro {
 
 
     public static void main(String[] args) {
+        loop();
+    }
 
+    /**
+     * Print the program header.
+     * Made to be identical to the example, as required by part (1)(5).
+     */
+    private static void printHeader() {
+        System.out.println(LINE_SEP);
+        System.out.println("^ Welcome to:");
+        System.out.println("^    BODY MASS INDEX (BMI) Computation PRO");
+        System.out.println("^               by Mos");
+        System.out.println("^");
+        System.out.println(LINE_SEP);
+    }
+
+    /**
+     * Run the program loop.
+     */
+    private static void loop() {
+        printHeader();
     }
 }
