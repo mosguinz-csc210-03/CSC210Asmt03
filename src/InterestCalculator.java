baa8c72ec16f374f1ad6df3a7c4388f59b2c03d9
mosguinz <mos.guinz@gmail.com>
2020-09-21 16:10:43 -0700

    Add print header method

---
+++ src/InterestCalculator.java
@@ -0,0 +1,17 @@
+public class InterestCalculator {
+    public static void main(String[] args) {
+        loop();
+    }
+
+    public static void loop() {
+        printHeader();
+    }
+
+    /**
+     * Print the program's header.
+     */
+    private static void printHeader() {
+        System.out.println("COMPOUND INTEREST CALCULATOR");
+        System.out.println("============================");
+    }
+}
