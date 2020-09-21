ee3690526c0c2a8d2d6375d1f8d03479805ee079
mosguinz <mos.guinz@gmail.com>
2020-09-20 17:25:18 -0700

    Add version selection for BMI Pro

---
+++ src/TableBmiPro.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class TableBmiPro {
+    public static void main(String[] args) {
+        System.out.print("Please enter 1 for the Standard version or 2 for the Pro version: ");
+        int option = new Scanner(System.in).nextInt();
+
+    }
+}
