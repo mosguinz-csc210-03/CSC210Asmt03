e8eeb04ad01d01e41709e6f1d50c0dbd4b4bf96b
mosguinz <mos.guinz@gmail.com>
2020-09-21 16:13:47 -0700

    Add file header

--- src/InterestCalculator.java
+++ src/InterestCalculator.java
@@ -1,3 +1,13 @@
+/*
+ * Assignment 03
+ * Description: [Part 3 of 3] Compound interest calculator
+ * Name: Kullathon "Mos" Sitthisarnwattanachai
+ * Teammate: Amber Hartigan
+ * ID: 921425216
+ * Class: CSC 210-03
+ * Semester: Fall 2020
+ */
+
 public class InterestCalculator {
     public static void main(String[] args) {
         loop();
