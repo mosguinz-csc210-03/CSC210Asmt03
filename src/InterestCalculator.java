8bbd374b6e275655f6551bdd8ee2424edd2abbc7
mosguinz <mos.guinz@gmail.com>
2020-09-21 20:43:20 -0700

    Add more comments to comply with (4)(b)

--- src/InterestCalculator.java
+++ src/InterestCalculator.java
@@ -21,17 +21,20 @@ public class InterestCalculator {
     public static void loop() {
         printHeader();
 
+        /* Create a Scanner object. */
         Scanner scan = new Scanner(System.in);
 
+        /* Prompt and scan the user's input for the deposit parameters. */
         System.out.print("Enter your principal deposit amount (in $): ");
         double principalSum = scan.nextDouble();
         System.out.print("Enter your compounding frequency (per annum): ");
         double compoundFreq = scan.nextDouble();
         System.out.print("Enter your compound annual interest rate (in %): ");
-        double interestRate = scan.nextDouble() / 100;
+        double interestRate = scan.nextDouble() / 100;  // convert percent to decimal
         System.out.print("Enter your deposit term (in years): ");
         double depositTerm = scan.nextDouble();
 
+        /* Display the report. */
         displayReport(principalSum, compoundFreq, interestRate, depositTerm);
     }
 
@@ -63,23 +66,32 @@ public class InterestCalculator {
 
         /* Show a quarterly report if the deposit term is 2 years or less. */
         double reportFrequency = depositTerm <= 2 ? 0.25 : 1;
+
+        /* Re-summarize the user's parameters in words. */
         System.out.println("\n" + "=".repeat(56));
         System.out.printf("Your %s compound interest report:%n", reportFrequency == 1 ? "yearly" : "quarterly");
         System.out.printf("Principal sum of $%.2f with a %.2f-year deposit term%n", principalSum, depositTerm);
         System.out.printf("compounding %.0f time(s) per year at %.2f%% p.a.%n", compoundFreq, interestRate * 100);
         System.out.println("=".repeat(56));
+
+        /* Print the column headers. */
         System.out.printf(" %7s │ %12s │ %12s │ %12s%n", "Deposit", "Interest", "Cumulative", "New");
         System.out.printf(" %7s │ %12s │ %12s │ %12s%n", "length", "at period", "interest", "principal");
         System.out.println("-".repeat(56));
+
+        /* Generate the table. */
         double newSum, prevAccumInterest = 0, currentInterest, accumInterest;
         for (double length = 0; length <= depositTerm; length += reportFrequency) {
             newSum = calculateNewPrincipal(principalSum, compoundFreq, interestRate, length);
-            accumInterest = newSum - principalSum;
-            currentInterest = accumInterest - prevAccumInterest;
+            accumInterest = newSum - principalSum;                // total generated interest
+            currentInterest = accumInterest - prevAccumInterest;  // interest generated at this period
+
+            /* Print the row for this period. */
             System.out.printf("%,5.2f yr │ $ %,10.2f │ $ %,10.2f │ $ %,10.2f%n", length, currentInterest, accumInterest, newSum);
             prevAccumInterest = accumInterest;
         }
 
+        /* Re-calculate the final principal balance and print the summary. */
         double finalSum = calculateNewPrincipal(principalSum, compoundFreq, interestRate, depositTerm);
         System.out.println("=".repeat(56));
         System.out.printf("%23s: $ %,10.2f%n", "Original principal sum", principalSum);
